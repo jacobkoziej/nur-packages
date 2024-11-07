1e2866c06525db0572d5ed26adce636c7d1329f8
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-06 23:48:47 -0500

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Jacob Koziej's NUR";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs:
+
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
