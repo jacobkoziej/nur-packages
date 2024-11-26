48cff72b6270aaf327d751a8e9372af8b3480d45
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-26 18:31:36 -0500

    Add `pkgs.espressif.openocd-esp32`

--- pkgs/espressif/default.nix
+++ pkgs/espressif/default.nix
@@ -3,4 +3,5 @@
 lib.attrsets.recurseIntoAttrs {
   binutils-gdb = import ./binutils-gdb { inherit pkgs lib; };
   crosstool-ng = import ./crosstool-ng { inherit pkgs lib; };
+  openocd-esp32 = import ./openocd-esp32 { inherit pkgs; };
 }
---
+++ pkgs/espressif/openocd-esp32/sha256.nix
@@ -0,0 +1,5 @@
+{
+  "0.12.0-esp32-20241016" = {
+    "linux-amd64" = "sha256-6CsPA23JkkS+rV8JqG6RuyNly80RIqxoJh5WR5Qkhd8=";
+  };
+}
