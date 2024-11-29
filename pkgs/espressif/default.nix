30b218e829d480e58927a2828ec030dd6db22abe
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-29 00:37:50 -0500

    Add `pkgs.espressif.esp-coredump`

--- pkgs/espressif/default.nix
+++ pkgs/espressif/default.nix
@@ -4,6 +4,7 @@ lib.attrsets.recurseIntoAttrs {
   binutils-esp32ulp = import ./binutils-esp32ulp { inherit pkgs; };
   binutils-gdb = import ./binutils-gdb { inherit pkgs; };
   crosstool-ng = import ./crosstool-ng { inherit pkgs; };
+  esp-coredump = pkgs.callPackage ./esp-coredump.nix { };
   idf-component-manager = pkgs.callPackage ./idf-component-manager.nix { };
   openocd-esp32 = import ./openocd-esp32 { inherit pkgs; };
 }
