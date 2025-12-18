d97d4e6cf0546b8bf001d92d2159a5dc1c9bb44a
Jacob Koziej <jacobkoziej@gmail.com>
2025-12-17 23:37:30 -0800

    chore: remove support for `espressif`

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,5 @@
 
 lib.attrsets.recurseIntoAttrs {
   analog-devices = import ./analog-devices { inherit pkgs lib; };
-  espressif = import ./espressif { inherit pkgs lib; };
   raspberry-pi = import ./raspberry-pi { inherit pkgs lib; };
 }
--- pkgs/espressif/binutils-gdb/sha256.nix
+++
@@ -1,7 +0,0 @@
-{
-  "14.2_20240403" = {
-    "xtensa-esp-elf-gdb" = {
-      "x86_64-linux" = "sha256-nWhHLUy6XPjCt52U+G+SyCjnamMr0ea+XncG5bME024=";
-    };
-  };
-}
