3b197bde2b7dd1b806864e9d7c45116cffd0f25f
Jacob Koziej <jacobkoziej@gmail.com>
2025-12-25 23:02:40 -0500

    refactor: remove `srcOverride` parameter

--- pkgs/com/github/openocd-org/openocd/default.nix
+++ pkgs/com/github/openocd-org/openocd/default.nix
@@ -7,30 +7,23 @@
   openocd,
   which,
 
-  srcOverride ? null,
   enableFtdi ? true,
   extraHardwareSupport ? [ ],
 }:
 
 let
-  inherit (lib.strings) optionalString;
-
-  defaultSrc = fetchFromGitHub {
-    owner = "openocd-org";
-    repo = "openocd";
-    rev = "e440b0648fd5a9ced8b3a05ff1a73b17c00cd144";
-    hash = "sha256-0DcEQv2ZBs61d4xlT5iI1i4tTyIVJah5aw3Ke1Zz34c=";
-  };
-
-  src = if srcOverride != null then srcOverride else defaultSrc;
-
-  rev = src.rev or "";
+  rev = "e440b0648fd5a9ced8b3a05ff1a73b17c00cd144";
 
 in
 (openocd.overrideAttrs (previousAttrs: {
-  version = previousAttrs.version + optionalString (rev != "") "-${rev}";
+  version = rev;
 
-  inherit src;
+  src = fetchFromGitHub {
+    owner = "openocd-org";
+    repo = "openocd";
+    inherit rev;
+    hash = "sha256-0DcEQv2ZBs61d4xlT5iI1i4tTyIVJah5aw3Ke1Zz34c=";
+  };
 
   patches = [
     ./src-rev.patch
